d304402524fb88f945f41449e5a94d10be93e850
Dang Nguyen <dwangdnguyen@gmail.com>
2022-05-21 09:44:32 +0700

    Layout-Default

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,9 @@
-function Sidebar() {
-    return <h1>Sidebar</h1>;
-}
+import styles from './Sidebar.module.scss';
+import classNames from 'classnames/bind';
 
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+    return <aside className={cx('wrapper')}></aside>;
+}
 export default Sidebar;
